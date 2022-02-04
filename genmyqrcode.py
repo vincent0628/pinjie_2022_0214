48a89bbf0ad2b9093c2ceab47b7f4391c002e8e0
Vincent <vincent0910628007@gmail.com>
2022-02-04 19:54:01 +0800

    change QR code .py

--- genmyqrcode.py
+++ genmyqrcode.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python3
 import os
 from MyQR import myqr
 version, level, qr_name = myqr.run(
-	'https://jackluson.github.io/to-lover-demo/',
-	# 'https://jackluson.github.io/to-lover/',
+	'https://vincent0628.github.io/pinjie_2022_0214/',
     version=1,
     level='H',
     picture='pic-icon.png',
     colorized=True,
     contrast=1.0,
     brightness=1.0,
-    save_name='ip_jackLu.png',
+    save_name='ip_pinjie_2022_0214.png',
     save_dir=os.getcwd()
 )
 print('version',version)
